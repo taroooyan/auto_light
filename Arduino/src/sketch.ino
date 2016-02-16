20f338ce0711f1c924dcd031b04b7e8e8787ca96
taroooyan <taroooyan@gmail.com>
2016-02-17 04:46:07 +0900

    シリアル通信により制御する

--- Arduino/src/sketch.ino
+++ Arduino/src/sketch.ino
@@ -1,59 +1,37 @@
 #include <Servo.h>
-const int PIRpin = 7;
 const int SERVOpin = 9;
 Servo servo0;
 
 void setup() { 
   Serial.begin(9600);   // シリアル接続を9600に設定
   servo0.attach(SERVOpin);//D9にサーボの信号線を接続
-  //initialize
-  servo0.write(-5);
-  delay(1000);
-  servo0.write(25);
-  delay(1000);
+  servo0.write(20);
+  delay(500);
 }
 
-int fon = 0;
-int foff = 0;
-int human_sensor(void);
-int human_sensor() {
-  while(1){
-    Serial.print(digitalRead(PIRpin));
-    // 反応しているときLOW
-    if (digitalRead(PIRpin) == LOW){
-      fon++;
-    }
-    if (digitalRead(PIRpin) == HIGH){
-      foff++;
-    }
-    if(fon > 7){
-      Serial.println("点灯");
-      foff = 0;
-      fon = 0;
-      return 1;
-    }
-    // 5分反応しなかったら消灯
-    if(foff > 300){
-      Serial.println("消灯");
-      foff = 0;
-      fon = 0;
-      return 0;
-    }
-    delay(100);
-  }
-}
-
+byte command;
 void loop(){
-  int juage = human_sensor();
-  //消す
-  if (juage == 0){
-    servo0.write(-5);
-    //誤作動があるため
-    delay(5000);
-  }
-  //点ける
-  else{
-    servo0.write(25);
-    delay(500);
+  if(Serial.available() > 0){
+    //消す
+    command = Serial.read();
+    if (command == '0'){
+      servo0.attach(SERVOpin);//D9にサーボの信号線を接続
+      delay(20);
+      servo0.write(0);
+      //誤作動があるため
+      delay(500);
+      servo0.detach();
+      delay(5000);
+    }
+    //点ける
+    else if (command == '1'){
+      servo0.attach(SERVOpin);//D9にサーボの信号線を接続
+      delay(20);
+      servo0.write(18);
+      //誤作動があるため
+      delay(500);
+      servo0.detach();
+      delay(5000);
+    }
   }
 }
