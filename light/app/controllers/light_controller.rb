a7cb5f771de51271cc6120bd923a9fca684ec1aa
taroooyan <taroooyan@gmail.com>
2016-04-23 21:39:34 +0900

    コメントアウトを外す

--- light/app/controllers/light_controller.rb
+++ light/app/controllers/light_controller.rb
@@ -4,27 +4,27 @@ class LightController < ApplicationController
 
   def on
     render text: "証明がONになりました"
-    # str = '1'
-    # com = open("/dev/ttyACM0","r+")
-    # system(" stty < /dev/ttyACM0 9600")
-    # sleep(2)
-    # com.write str
-    # sleep(2)
-    # com.write 'end'
-    # sleep(2)
-    # com.close()
+    str = '1'
+    com = open("/dev/ttyACM0","r+")
+    system(" stty < /dev/ttyACM0 9600")
+    sleep(2)
+    com.write str
+    sleep(2)
+    com.write 'end'
+    sleep(2)
+    com.close()
   end
 
   def off
     render text: "証明がOFFになりました"
-    # str = '1'
-    # com = open("/dev/ttyACM0","r+")
-    # system(" stty < /dev/ttyACM0 9600")
-    # sleep(2)
-    # com.write str
-    # sleep(2)
-    # com.write 'end'
-    # sleep(2)
-    # com.close()
+    str = '1'
+    com = open("/dev/ttyACM0","r+")
+    system(" stty < /dev/ttyACM0 9600")
+    sleep(2)
+    com.write str
+    sleep(2)
+    com.write 'end'
+    sleep(2)
+    com.close()
   end
 end
